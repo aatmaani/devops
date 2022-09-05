df6248494e3378b4e745d524f67fc0e5a7b0a44c
aatmaani <111965953+aatmaani@users.noreply.github.com>
2022-09-05 14:59:52 +0530

    Update Dockerpushtoecr.sh

--- Dockerpushtoecr.sh
+++ Dockerpushtoecr.sh
@@ -4,7 +4,7 @@ aws ecr get-login-password --region ap-south-1 | docker login --username AWS --p
 #sudo  docker tag 123 215767047920.dkr.ecr.ap-south-1.amazonaws.com/project/nodejs-app:v1 
 #sudo  docker push 215767047920.dkr.ecr.ap-south-1.amazonaws.com/project/nodejs-app:v1 
 
-docker tag project/nodejs-app:latest 215767047920.dkr.ecr.ap-south-1.amazonaws.com/project/nodejs-app:$BUILD_NUMBER
+docker tag project/nodejs-app:$BUILD_NUMBER 215767047920.dkr.ecr.ap-south-1.amazonaws.com/project/nodejs-app:$BUILD_NUMBER
 
 docker push 215767047920.dkr.ecr.ap-south-1.amazonaws.com/project/nodejs-app:$BUILD_NUMBER
 
