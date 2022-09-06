2e0c21724d941143d4c9c1a217207c20f6909cc9
root <root@ip-172-31-34-118.ap-south-1.compute.internal>
2022-09-06 11:02:59 +0000

    latestdev

--- Dev.sh
+++ Dev.sh
@@ -6,17 +6,17 @@ cd Dev-v1
 git clone https://github.com/aatmaani/project.git
 
 cd project
-docker build -t project/nodejs-app:Dev-v1 .
+docker build -t project/nodejs-app:latestDev .
 
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 215767047920.dkr.ecr.ap-south-1.amazonaws.com
 #sudo  docker tag 123 215767047920.dkr.ecr.ap-south-1.amazonaws.com/project/nodejs-app:v1 
 #sudo  docker push 215767047920.dkr.ecr.ap-south-1.amazonaws.com/project/nodejs-app:v1 
 
-docker tag project/nodejs-app:Dev-v1 215767047920.dkr.ecr.ap-south-1.amazonaws.com/project/nodejs-app:Dev-v1
+docker tag project/nodejs-app:latestDev 215767047920.dkr.ecr.ap-south-1.amazonaws.com/project/nodejs-app:latestDev
 
-docker push 215767047920.dkr.ecr.ap-south-1.amazonaws.com/project/nodejs-app:Dev-v1
+docker push 215767047920.dkr.ecr.ap-south-1.amazonaws.com/project/nodejs-app:latestDev
 cd
 cd workspace/123/charts
 
-helm upgrade --install nodejs-dev nodejs -f values-dev.yaml --set image.tag=Dev-v1 -n dev
+helm upgrade --install nodejs-dev nodejs -f values-dev.yaml --set image.tag=latestDev -n dev
 
