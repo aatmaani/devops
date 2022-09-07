2923b6a54e021ea24a00f683c9be40d900ee8d6e
root <root@ip-172-31-34-118.ap-south-1.compute.internal>
2022-09-07 04:11:56 +0000

    latestQA

--- prod.sh
+++ prod.sh
@@ -1,10 +1,15 @@
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 215767047920.dkr.ecr.ap-south-1.amazonaws.com
 
-docker pull 215767047920.dkr.ecr.ap-south-1.amazonaws.com/project/nodejs-app:qalatest
+docker pull 215767047920.dkr.ecr.ap-south-1.amazonaws.com/project/nodejs-app:latestQA
 
-docker tag 215767047920.dkr.ecr.ap-south-1.amazonaws.com/project/nodejs-app 215767047920.dkr.ecr.ap-south-1.amazonaws.com/project/nodejs-app:prodlatest
+docker tag 215767047920.dkr.ecr.ap-south-1.amazonaws.com/project/nodejs-app 215767047920.dkr.ecr.ap-south-1.amazonaws.com/project/nodejs-app:prod-v1
 
-docker push 215767047920.dkr.ecr.ap-south-1.amazonaws.com/project/nodejs-app:prodlatest
+docker push 215767047920.dkr.ecr.ap-south-1.amazonaws.com/project/nodejs-app:prod-v1
 
 aws ecr describe-images --repository-name project/nodejs-app
+
+cd
+cd workspace/Dev/charts/
+
+helm upgrade --install nodejs-prod nodejs -f values-prod.yaml --set image.tag=prod-v1 -n prod
 
